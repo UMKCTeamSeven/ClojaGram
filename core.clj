1322c2b51076a21b75e5007c36a1e5d10a656dbe
VhatAmI <heavenyale@gmail.com>
2015-12-13 15:12:05 -0600

    Checkpoint functions forming

--- core.clj
+++ core.clj
@@ -4,63 +4,56 @@
 (require '[clojure.java [io :as io]])
 (use 'clojure.string)
 ;;I assume we aren't allowed to use this ? https://github.com/Engelberg/instaparse
-(defn tokkens
-      #{
-         {  :name :id      :pattern #"\w+"  :priority -1 }  ;; Catches case sensitive IDs
-         {  :name :minus   :pattern "-" }
-         {  :name :assign  :pattern "=" }
-         {  :name :divide  :pattern "/" }
-         {  :name :plus    :pattern "+" }
-         {  :name :mult    :pattern "*" }
-         {  :name :lparen  :pattern "(" }
-         {  :name :rparen  :pattern ")" }
-         {  :name :int     :pattern #"\d+" }
-         {  :ignore true   :pattern #"\s+" }})
-
 (with-open [r (reader "input.txt")]
    (doseq [line (line-seq r)]
       (spit "output.txt" (str (join "\n" (split line #"\s+")) "\n") :append true)
    )
 )
-
 (println (slurp "output.txt"))
 ;;output.txt contains each token.
 ;; lines without the *** are the last tooken in a line.
 (spit "tokken.txt" (reduce conj #{} (line-seq (io/reader "output.txt"))) :append true)
 ;;We now have a set of each lexeme from the input, no duplicates. We need to match these to tokkens
-;; (c) seems to be a special case that will need to be broken down further.
-
-    ;; asked about (c) verified it was supposed to be ( c )
-
 ;;17 possible tokkens in given input file
 
-;;Now that we have def tokkens and tokken.txt we will combine them to get a parse table
+;;Now that we have def tokkens and tokken.txt we will combine them to get a parse table?? But first we need to define the grammar checking functions
 
 ;; Reference : http://stackoverflow.com/questions/19412624/pass-multiple-parameters-function-from-other-function-with-clojure-and-readabili
 ;; Reference : http://clojuredocs.org/clojure.core/apply
 ;; Reference :  http://clojure.org/cheatsheet
+;; good god... this syntax is hard.
 
-;; good god... this is hard.
-;;Given Gramar;;
-
-(defn findcloseparen [x]
-
+;;helper functions
+(defn findcloseparen [x] ;;To do
+  ;; until end of line look for )
+  )
+(defn isAWrappedExpression [theexpression]   ;; TO FINISH
+  (if (re-matches "("))
+  (findcloseparen theexpression)  ;; No close paren? then not a FACTOR
+    ;;way to call on inner paren area only
   )
 
+(defn isATailTerm [theterm]   ;TO DO
+
+  )                                                                                          ;;ADD REJECTS LAST SINCE EXTRA CREDI LOGIC WILL NEED TO BE CONSIDERED
+;;Given Gramar;;
 ;; EXPRESSION ->  TERM  {  ( + | - ) TERM }
 (defn isAExpression [theexpression]
-  (if (re-matches "("))
-  (findcloseparen theexpression)  ;; No close paren? then not an expression
-
-
+   if(or (isATerm theexpression) (isATailTerm theexpression))
+      "ACCEPT"
   )
 ;; TERM -> FACTOR { ( * | / ) FACTOR }
-(defn isATerm )
+(defn isATerm [theterm]
+  if (or (isAFactor theterm) (isATailFactor theterm))
+  "ACCEPT"
+  )
 ;; FACTOR -> ID  |  INT  | (EXPRESSION)
-(defn isAFactor [thetokken]
-  (if (or (re-matches #"\w+" thetokken) (re-matches #"\d+" thetokken))
+(defn isAFactor [thefactor]
+  (if (or (re-matches #"\w+" thefactor) (re-matches #"\d+" thefactor))
     "ACCEPT")
-  (isAExpression thetokken)
+  (if (isWrappedExpression thetokken))
+    "ACCEPT"
+
   )
 ;; Addtional details ---- 0 length line end point
 
