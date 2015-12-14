b7b604862c7f0398271604d94e0e2d0bc1fa0a68
VhatAmI <heavenyale@gmail.com>
2015-12-13 22:19:10 -0600

    Added GrammarCHECK -- Exception on line 16

--- core.clj
+++ core.clj
@@ -1,4 +1,4 @@
-;;Matt Yale
+;;Matt Yale      ;; will need this later (spit "results.txt" :append true)
 ;;Fall 2015
 (use 'clojure.java.io)
 (require '[clojure.java [io :as io]])
@@ -7,33 +7,53 @@
 (declare isATerm)
 (declare isAFactor)
 (declare isWrappedExpression)
+(declare grammarCHECK)
 
+(defn grammarCHECK [aline]
+
+  (def thecount 0)
+  (let [nothingfound (nth (split aline #"\s+") (thecount) "nothing found")])
+  (while    (if-not (= nothingfound "nothing found") true false)
+    (print thecount)(print "debugging function")
+    (isAFactor (nth (split aline #"\s+") (thecount) "nothing found"))
+
+
+
+    (def i++ (inc thecount))
+    (i++)
+    (let [nothingfound (nth (split aline #"\s+") (thecount) "nothing found")])
+    )
+  )
 
 (with-open [r (reader "input.txt")]
    (doseq [line (line-seq r)]
       (spit "output.txt" (str (join "\n" (split line #"\s+")) "\n") :append true)
    )
 )
-(println (slurp "output.txt"))
+;;(println (slurp "output.txt"))
 ;;output.txt contains each token.
 
 ;; lines without the *** are the last tooken in a line.
-(spit "tokken.txt" (reduce conj #{} (line-seq (io/reader "output.txt"))) :append true)
+(spit "tokken.txt" (reduce conj #{} (line-seq (io/reader "output.txt"))) :append true)    ;;ended up not using tokkens to make a parse table
+;;We now have a set of each lexeme from the input, no duplicates. We need to match these to tokkens
+;;17 possible tokkens in given input file
+
+
+
+(grammarCHECK    (line-seq(io/reader "input.txt")))
+
+
 
 ;;helper functions
-(defn findcloseparen [x] ;;To do
-  ;; until end of line look for )
-  )
-(defn isAWrappedExpression [theexpression]   ;; TO FINISH
-  (if (re-matches "(") theexpression)
-  (findcloseparen theexpression)  ;; No close paren? then not a FACTOR
-    ;;way to call on inner paren area only
-  )
+(defn isCloseParen [x] ;;To do
 
+  )
+(defn isOpenParen [theexpression]   ;; TO FINISH
+  (if (re-matches "(") theexpression)
+  )
 (defn isATailTerm [theterm]   ;TO DO
 
   )
-
 (defn isATailFactor [thefactor]
 
   )
@@ -48,37 +68,9 @@
   )
 ;; FACTOR -> ID  |  INT  | (EXPRESSION)  ;COMPLETE
 (defn isAFactor [thefactor]
-  (if (or (re-matches #"\w+" thefactor) (re-matches #"\d+" thefactor)) true)
-
-  (if (isWrappedExpression thefactor) true false)
+  (if (or (re-matches #"\w+" thefactor) (re-matches #"\d+" thefactor)) true  (if (isOpenParen thefactor) true false))
   )
-
-
-;;We now have a set of each lexeme from the input, no duplicates. We need to match these to tokkens
-;;17 possible tokkens in given input file
-
-;;Now that we have def tokkens and tokken.txt we will combine them to get a parse table?? But first we need to define the grammar checking functions
-
 ;; Reference : http://stackoverflow.com/questions/19412624/pass-multiple-parameters-function-from-other-function-with-clojure-and-readabili
 ;; Reference : http://clojuredocs.org/clojure.core/apply
 ;; Reference :  http://clojure.org/cheatsheet
 ;; good god... this syntax is hard.
-;; Addtional details ---- 0 length line end point
-
-;;                   ---- All ID will be 1 char in length, they are case sensitive
-
-;;                   ---- All INT will be >= 0
-
-;;                   ---- no unary minus
-
-;; Output results of each line to screen "ACCEPT or REJECT"
-
-;; Extra credit, why a line is rejected, reason due to grammar
-
-
-
-
-
-
-
-
