f0ac453f1796e75b435f32282ae399c3b7d17929
VhatAmI <heavenyale@gmail.com>
2015-12-14 05:31:25 -0600

    null ref at line 71
    Uncertain as to why

--- core.clj
+++ core.clj
@@ -22,43 +22,56 @@
      true false)
   )
 (defn isATailTerm [theterm]
-
+  (if (or (re-matches #"\+" theterm) (re-matches #"\-" theterm)) true false
+    )
   )
 (defn isATailFactor [thefactor]
-
+  (if (or (re-matches #"\*" thefactor) (re-matches #"\/" thefactor)) true false
+    )
   )
 ;;Given Gramar;;
 ;; EXPRESSION ->  TERM  {  ( + | - ) TERM }
 (defn isAExpression [theexpression]
-  (if(or (isATerm theexpression) (isATailTerm theexpression)) true false)
+  (if (isATerm theexpression)  true false)
   )
 ;; TERM -> FACTOR { ( * | / ) FACTOR }
 (defn isATerm [theterm]
-  (if (or (isAFactor theterm) (isATailFactor theterm)) true false)
+  (if (isAFactor theterm)  true false)
   )
 ;; FACTOR -> ID  |  INT  | (EXPRESSION)  ;COMPLETE
 (defn isAFactor [thefactor]
   (if (or (re-matches #"\w+" thefactor) (re-matches #"\d+" thefactor)) true  (if (isOpenParen thefactor) true false))
   )
 (defn grammarCHECK [aline]
-  (let [tokkentype ""])
+
   (defn linetravel [aline thecount]
     (nth (split aline #"\s+") thecount "nothing found")
     )
-  (loop [thecount 0]
+  (loop [thecount 0 tokkentype ""]
     (when (not= (linetravel aline thecount) "nothing found")
-    (do
-      (print "debugging function")
-      (if (isAFactor (linetravel aline thecount)) (if (isOpenParen (linetravel aline thecount)) (let [tokkentype "("]) (let [tokkentype "FACTOR"]) ) false)
-      (if (isATerm (linetravel aline thecount)) true false)
-      (if (isAExpression (linetravel aline thecount)) true false)
+      (do
 
-  )(recur (inc thecount)))
-    ))
+        (if (= tokkentype "") ( (if (isAFactor (linetravel aline thecount)) (if (isOpenParen (linetravel aline thecount)) (let [tokkentype "("]) (let [tokkentype "FACTOR"])) false)
+                                (if (isATerm (linetravel aline thecount)) (let [tokkentype "TERM"])) (if (isATailFactor (linetravel aline thecount)) (let [tokkentype (linetravel aline thecount)]) false)
+                                (if (isAExpression (linetravel aline thecount)) (let [tokkentype "EXPRESSION"])) (if (isATailTerm (linetravel aline thecount)) (let [tokkentype (linetravel aline thecount)]) false))
+          ((if (= tokkentype "EXPRESSION") (spit "result.txt" "ACCEPT" :append true) false)
+        (if (= tokkentype "FACTOR")    (if (isAExpression(linetravel aline thecount)) (let [tokkentype (linetravel aline thecount)]) (if (isATailFactor (linetravel aline thecount)) (let [tokkentype (linetravel aline thecount)])
+                                                                                                                                     (print "REJECT, Factor not followed by tailfactor" )) ))
+        (if (= tokkentype "TERM")      (if (isATailTerm (linetravel aline thecount)) (let [tokkentype (linetravel aline thecount)])
+                                                                                                                                       (print "REJECT, term not followed by tailterm" )) )
+        (if (or (re-matches #"\+" tokkentype) (re-matches #"\-" tokkentype)) (if (isATerm (linetravel aline thecount)) (spit "result.txt" "ACCEPT" :append true) (print "REJECT, + | - not followed by TERM")))
+          (if (or (re-matches #"\/" tokkentype) (re-matches #"\*" tokkentype)) (if (isAFactor(linetravel aline thecount))  (spit "result.txt" "ACCEPT" :append true) (print "REJECT, * | / not followed by FACTOR"))))
+          )
+
+          )
+
+
+  )(recur (inc thecount) (let [tokkentype tokkentype] )))
+    )
 (with-open [r (reader "input.txt")]
    (doseq [line (line-seq r)]
 
-      (spit "results.txt"  (grammarCHECK line))
+      (spit "results.txt" (grammarCHECK line))
       (spit "output.txt" (str (join "\n" (split line #"\s+")) "\n") :append true)
    )
 )
